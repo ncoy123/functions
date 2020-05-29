736dd9a806a3a59e4ccbe9f413d208ccc2fefd16
Nicholas Coy <ncoy@ncoy-ma.lan>
2020-05-29 14:11:57 -0700

    adding non-parametric tests

--- stats_analysis.py
+++ stats_analysis.py
@@ -4,12 +4,24 @@ from scipy.stats import shapiro
 from scipy.stats import normaltest
 import seaborn as sns 
 import matplotlib.pyplot as plt
+from scipy.stats import mannwhitneyu
+from scipy.stats import ttest_rel
+from scipy.stats import wilcoxon
+
+#functions for statistical analysis for inferential stats
+#parametric and nonparametric 
+#functions for plotting distributions
+#checks distribution of samples for normality
+#one and two tailed tests for determing if smaples came from different distributions
+
+###Parametric Tests###
 
 #check sample for normal distribution using shapiro-wilk test and D'Agostinos's K^2 test
 #note gaussian means normal distribution 
+#input is sample array
 def check_normality(sample):
-    #using an alpha of 5% for 95% confidence
-    alpha = 0.05
+    #using an alpha of 10% for 90% confidence
+    alpha = 0.1
     print('H0: sample is normally distributed')
     print('H1: sample is not normally distributed')
     print('Sample size=%.3f' % (len(sample)))
@@ -37,6 +49,7 @@ def check_normality(sample):
 #t-test, two tailed for difference of means between two independent samples
 #alpha of 5% for 95% confidence
 #not assuming equal variance in samples
+#input are two arrays to be tested
 def students_two_tailed(sample_1, sample_2):
     print('H0: sample 1 mean = sample 2 mean')
     print('H1: sample 1 mean != sample 2 mean')
@@ -52,10 +65,11 @@ def students_two_tailed(sample_1, sample_2):
         print('reject H0, samples means are not equal')
 
 
-#t-test, one tailed test to see if sample 1 mean > sample 2 mean
+#t-test, one tailed test to test if sample 1 mean > sample 2 mean
 #using standard alpha of 5% for 95% confidence
 #scipy.stats.ttest_ind is a two tailed test, so we divide the p-value(probability)/2 for one tailed test result
 #not assuming equal variance in samples
+#input are two arrays to be tested
 def students_one_tailed(sample_1, sample_2):
     print('H0: sample 1 mean <= sample 2 mean')
     print('H1: sample 1 mean > sample 2 mean')
@@ -71,6 +85,110 @@ def students_one_tailed(sample_1, sample_2):
     else:
         print('reject H0, sample 1 mean > sample 2 mean')
 
+#paired t-test, two tailed for difference between means of two dependent samples
+#alpha of 5% for 95% confidence
+#inputs are two arrrays to be tested
+def paired_two_tail(sample_1, sample_2):
+    print('H0: sample 1 mean = sample 2 mean')
+    print('H1: sample 1 mean != sample 2 mean')
+    alpha = 0.05
+    print('sample 1: mean=%.3f stdv=%.3f' % (np.mean(sample_1), np.std(sample_1)))
+    print('sample 2: mean=%.3f stdv=%.3f' % (np.mean(sample_2), np.std(sample_2)))
+    stat, p = ttest_rel(sample_1, sample_2)
+    print('Using an alpha of: %.2f' % (alpha))
+    print('T Statistics=%.3f, p=%.3f' % (stat, p))
+    if p > alpha:
+	    print('fail to reject H0, sample means are equal')
+    else:
+	    print('reject H0, samples means are not equal')
+
+#paired t-test, one tailed to test if sample 1 mean > sample 2
+#alpha of 5% for 95% confidence
+#inputs are two arrrays to be tested
+def paired_one_tail(sample_1, sample_2):
+    print('H0: sample 1 mean <= sample 2 mean')
+    print('H1: sample 1 mean > sample 2 mean')
+    alpha = 0.05
+    print('sample 1: mean=%.3f stdv=%.3f' % (np.mean(sample_1), np.std(sample_1)))
+    print('sample 2: mean=%.3f stdv=%.3f' % (np.mean(sample_2), np.std(sample_2)))
+    stat, p = ttest_rel(sample_1, sample_2)
+    p_2 = p/2
+    print('Using an alpha of: %.2f' % (alpha))
+    print('T Statistics=%.3f, p=%.3f' % (stat, p_2))
+    if p_2 > alpha:
+        print('fail to reject H0, sample 1 mean <= sample 2 mean')
+    else:
+        print('reject H0, sample 1 mean > sample 2 mean')
+
+###Non Parametric Tests###
+#function for nonparametric Mann-Whitney U test
+#used to test two independent samples that do not fit nomral distributions
+#inputs are two sample arrays
+def u_test_two_tailed(sample_1, sample_2):
+    print('H0: sample 1 mean = sample 2 mean')
+    print('H1: sample 1 mean != sample 2 mean')
+    alpha = 0.05
+    stat, p = mannwhitneyu(sample_1, sample_2, alternative = 'two-sided')
+    print('sample 1: mean=%.3f stdv=%.3f' % (np.mean(sample_1), np.std(sample_1)))
+    print('sample 2: mean=%.3f stdv=%.3f' % (np.mean(sample_2), np.std(sample_2)))
+    print('Using an alpha of: %.2f' % (alpha))
+    print('U Statistics=%.3f, p=%.3f' % (stat, p))
+    if p > alpha:
+	    print('fail to reject H0, sample means are equal')
+    else:
+	    print('reject H0, samples means are not equal')
+
+#one sided greater than Mann-Whitney U test
+#used to test if sample 1 mean > sample 2 mean
+#inputs are two sample arrays
+def u_test_one_tailed(sample_1, sample_2):
+    print('H0: sample 1 mean <= sample 2 mean')
+    print('H1: sample 1 mean > sample 2 mean')
+    alpha = 0.05
+    stat, p = mannwhitneyu(sample_1, sample_2, alternative = 'greater')
+    print('sample 1: mean=%.3f stdv=%.3f' % (np.mean(sample_1), np.std(sample_1)))
+    print('sample 2: mean=%.3f stdv=%.3f' % (np.mean(sample_2), np.std(sample_2)))
+    print('Using an alpha of: %.2f' % (alpha))
+    print('U Statistics=%.3f, p=%.3f' % (stat, p))
+    if p > alpha:
+	    print('fail to reject H0, sample means are equal')
+    else:
+	    print('reject H0, samples means are not equal')
+
+#two tailed wilcoxon test 
+#used to test difference between two dependent samples
+#inputs are two sample arrays
+def wilcoxon_two_tailed(sample_1, sample_2):
+    print('H0: sample 1 median = sample 2 median')
+    print('H1: sample 1 median != sample 2 median')
+    alpha = 0.05
+    stat, p = wilcoxon(sample_1, sample_2)
+    print('sample 1: meadian=%.3f stdv=%.3f' % (np.median(sample_1), np.std(sample_1)))
+    print('sample 2: median=%.3f stdv=%.3f' % (np.median(sample_2), np.std(sample_2)))
+    print('Using an alpha of: %.2f' % (alpha))
+    print('W Statistics=%.3f, p=%.3f' % (stat, p))
+    if p > alpha:
+	    print('fail to reject H0, sample 1 median = sample 2 median')
+    else:
+	    print('reject H0, sample 1 median != sample 2 median')
+
+#one tailed wilcoxon test
+#test if sample 1 median is > sample 2
+#inputs are two sample arrays
+def wilcoxon_one_tailed(sample_1, sample_2):
+    print('H0: sample 1 median <= sample 2 median')
+    print('H1: sample 1 median > sample 2 median')
+    alpha = 0.05
+    stat, p = wilcoxon(sample_1, sample_2, alternative = 'greater')
+    print('sample 1: meadian=%.3f stdv=%.3f' % (np.median(sample_1), np.std(sample_1)))
+    print('sample 2: median=%.3f stdv=%.3f' % (np.median(sample_2), np.std(sample_2)))
+    print('Using an alpha of: %.2f' % (alpha))
+    print('W Statistics=%.3f, p=%.3f' % (stat, p))
+    if p > alpha:
+	    print('fail to reject H0, sample 1 median <= sample 2 median')
+    else:
+	    print('reject H0, sample 1 median > sample 2 median')
+
 #plot two seaborn distplots side by side
 #inputs are pandas series for experiment and control
 #font size for title font
@@ -80,3 +198,4 @@ def side_by_side_distplt(experiment_data, control_data, font_size):
     ax2.set_title('Control')
     sns.distplot(experiment_data, ax=ax1)
     sns.distplot(control_data, ax=ax2)
+
